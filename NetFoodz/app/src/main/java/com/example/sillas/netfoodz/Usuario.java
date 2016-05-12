5fd5ffe96212cd407ddc4129659379cfd83c1df7
JohnElyson <johnelyson@gmail.com>
2016-05-11 23:25:53 -0300

    Atualização - John

---
+++ NetFoodz/app/src/main/java/com/example/sillas/netfoodz/Usuario.java
@@ -0,0 +1,37 @@
+package com.example.sillas.netfoodz;
+
+/**
+ * Created by John on 05/05/2016.
+ */
+public class Usuario {
+    private Pessoa pessoa;
+    private String username;
+    private String senha;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Pessoa getPessoa() {
+        return this.pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+
+
+}
---
+++ NetFoodz/app/src/main/java/com/example/sillas/netfoodz/UsuarioLogado.java
@@ -0,0 +1,15 @@
+package com.example.sillas.netfoodz;
+
+/**
+ * Created by John on 05/05/2016.
+ */
+public class UsuarioLogado {
+    private Usuario usuario;
+
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+
+}
