d6b5d1e4357e5ae9fb6ad4e1c7a7fabe1ec2bd08
sillasphablo <sillinhas_phablo@hotmail.com>
2016-05-13 01:22:08 -0300

    Login Pelo Facebook
    Foi adicionado o Login ao facebook e depois será direcionado para a tela
    de Ofertas.

--- NetFoodz/app/src/main/java/com/example/sillas/netfoodz/Login.java
+++ NetFoodz/app/src/main/java/com/example/sillas/netfoodz/Login.java
@@ -1,24 +1,104 @@
 package com.example.sillas.netfoodz;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class Login extends AppCompatActivity {
-
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        facebookSDKInitialize();
         setContentView(R.layout.activity_login);
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        getLoginDetails(loginButton);
     }
-    public void ircadastrese(View view) {
-        Intent it = new Intent(Login.this, Cadastro.class);
-        startActivity(it);
+    /*
+     Initialize the facebook sdk.
+     And then callback manager will handle the login responses.
+    */
+    protected void facebookSDKInitialize() {
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
     }
+
+ /*
+  Register a callback function with LoginButton to respond to the login result.
+ */
+
+    protected void getLoginDetails(LoginButton login_button){
+
+        // Callback registration
+        login_button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult login_result) {
+                Intent intent = new Intent(Login.this,Cadastro.class);
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onCancel() {
+                // code for cancellation
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                //  code to handle error
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        Log.e("data",data.toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
     protected void onPause() {
-        // TODO Auto-generated method stub
         super.onPause();
-        finish();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
     }
 }
+
